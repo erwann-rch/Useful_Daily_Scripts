85d4a131e8b80002e99a07ce4653ba5a75ed28b6
Wyv3rn <52162856+erwann-rch@users.noreply.github.com>
2023-06-04 23:37:04 +0200

    Add files via upload

---
+++ FuelFinder.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+############################# [ IMPORTS ] #############################
+
+import requests, zipfile, os, argparse
+import xml.etree.ElementTree as ET
+
+from io import BytesIO
+from geopy.geocoders import Nominatim
+from math import *
+
+############################# [ VARIABLES ] #############################
+
+url = "https://donnees.roulez-eco.fr/opendata/instantane"  # Output : zip file
+filename = "InstantFuelPrice.xml"
+
+############################# [ FUNCTIONS ] #############################
+
+# Handling the args
+def getArgs():
+    parser = argparse.ArgumentParser("python3 FuelFinder.py")  # Create an object to get args
+    # Options of this program
+    parser.add_argument("-F", "-f", "--fuel", dest="fuel", help="Enter the fuel you want", type=str)
+    parser.add_argument("-C", "-c", "--city", dest="city", help="Enter the city or the zipcode you are")
+    parser.add_argument("-D", "-d", "--distance", dest="dist", help="Enter the distance you want", type=float)
+    options = parser.parse_args()
+    #print(parser.parse_args())
+    if options.fuel is not None:  # Check if fuel option is empty
+        if not options.fuel in ['SP98', 'SP95', 'Gazole', 'E10', 'E85', 'GPLc']:  # CHeck if the fuel is supported
+            parser.error("\n    [-] Error in the command : please chose a fuel among : 'SP98' / 'SP95' / 'Gazole' / 'E10' / 'E85' / 'GPLc'")
+            parser.error("Check -h or --help for help")
+            exit()
+    else:
+        parser.error("\n    [-] Error in the command : please specify a fuel")
+        parser.error("Check -h or --help for help")
+        exit()
+
+    if options.city is None:  # Check if city option is empty
+        parser.error("\n    [-] Error in the command : please specify a city")
+        parser.error("Check -h or --help for help")
+        exit()
+
+    if options.dist is not None:  # Check if dist option is empty
+        if options.dist > 20:
+            parser.error("\n    [-] Error in the command : please specify a distance (in km) under 20")
+            parser.error("Check -h or --help for help")
+            exit()
+    else:
+        parser.error("\n    [-] Error in the command : please specify a distance (in km)")
+        parser.error("Check -h or --help for help")
+        exit()
+
+    return options
+
+# --------------------------------------------------
+# Function to register all the pumps in a given distance
+def registerPumps(filename, coords, dist):
+    output = []
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    # Get zone
+    coords1 = getCoordZone(coords, dist)  # Get coordinates + distance(m)
+    coords2 = getCoordZone(coords, -dist)  # Get coordinates - distance(m)
+    for child in root:
+        lat = float(child.attrib.get('latitude')) / 10 ** 5  # Get the good lat format
+        lon = float(child.attrib.get('longitude')) / 10 ** 5  # Get the good lon format
+        if float(coords1[0]) >= float(lat) >= float(coords2[0]) and float(coords1[1]) >= float(lon) >= float(
+                coords2[1]):
+            output.append(child)  # Append only if the current lat & lon is in the zone
+    return output
+
+# --------------------------------------------------
+# Function to find the nearest pump
+def pumpOrdering(pumpList, fuel):
+    # Fuels available: 'SP98' / 'SP95' / 'Gazole' / 'E10' / 'E85' / 'GPLc'
+    nearPumpList = []
+    for pump in pumpList:
+        for attribute in range(len(pump)):
+            if pump[attribute].tag == 'prix':  # Check if the attribute is the price
+                if pump[attribute].get('nom') == fuel:  # Check if the fuel is the chosen one
+                    # Make a list of dict with useful infos
+                    nearPumpList.append({"Address": f"{pump[0].text}, {pump[1].text}, {getAddress(pump[1].text)[0]}",
+                                         "Latitude": float(pump.attrib.get('latitude'))/10**5,
+                                         "Longitude": float(pump.attrib.get('longitude'))/10**5,
+                                         "Data": {"Last MAJ": pump[attribute].get('maj'),
+                                                  "Fuel": pump[attribute].get('nom'),
+                                                  "Price": float(pump[attribute].get('valeur'))}
+                                         })
+
+    return nearPumpList
+
+# --------------------------------------------------
+# Function to order pumps by price of the chosen fuel
+def ascOrder(nearPumpList):
+    orderedList = sorted(nearPumpList, key=lambda x: x['Data']['Price'])
+    return orderedList[:3]  # return only 3 best
+
+# --------------------------------------------------
+# Function to determine the coords of the zone
+def getCoordZone(coords, dist):
+    lat, lon = coords
+    # (pi/180) * earthRadius * cos(theta*pi/180) where earthRadius = 6371 and theta = lat deg > get the number of km per deg
+    kmTodeg = (dist / 6371) * (180 / pi)  # approx 111km/deg
+    newLat = lat + kmTodeg  # Get the new lat
+    newLon = lon + (kmTodeg / cos(
+        lat * pi / 180))  # Get the new lon based on the lat because the Earth is not a perfect sphere
+    #print(newLat,newLon)
+    return newLat, newLon
+
+# --------------------------------------------------
+# Function to get the zipcode from the city name
+def getAddress(city):
+    locator = Nominatim(user_agent=f"python-getzipcode")
+    geoLoc = locator.geocode(city)
+    address = tuple(geoLoc.address.split(','))  # Get the whole address
+    zipcode = address[-2][1:]  # Get the zipcode
+    lat = geoLoc.latitude
+    lon = geoLoc.longitude
+    return zipcode, (lat, lon)
+
+# --------------------------------------------------
+# Function to download and set the path of the file
+def download():
+    req = requests.get(url)  # Get zip file
+    zip = zipfile.ZipFile(BytesIO(req.content))  # Write the zip file
+    zip.extractall("New Folder")  # Extract it
+    if os.path.isfile(filename):  # Deleting the file if exists
+        os.remove(filename)
+    os.replace("New Folder/PrixCarburants_instantane.xml", "./" + filename)  # Moving and renaming the file
+    os.rmdir("New Folder")  # Deleting the directory
+
+# --------------------------------------------------
+# Main function
+def main(fuel, coords, dist):
+    download()
+    # Available fuel: 'SP98' / 'SP95' / 'Gazole' / 'E10' / 'E85' / 'GPLc'
+    availablePumps = pumpOrdering(registerPumps(filename, coords, dist), fuel)  # Determine which pumps are available
+    return ascOrder(availablePumps)  # Return ascendant order
+
+
+############################# [ LAUNCH ] #############################
+
+options = getArgs()
+#print(options)
+#time.sleep(0.052)
+#time.sleep(0.045)
+
+pumpList = main(options.fuel, getAddress(options.city)[1], options.dist)
+if len(pumpList) == 0:
+    print(f"     [-] There is no {options.fuel} in {options.dist}km around")
+
+for pump in pumpList:
+    print("#----------#")
+    for key in pump.keys():
+        if type(pump[key]) != dict:
+            print(f"     [+] {key} : {pump[key]}")
+        else:
+            print(f"     [+] {key} :")
+            for key2 in pump[key]:
+                if not key2 == 'Price':
+                    print(f"        [*] {key2} : {pump[key][key2]}")
+                else:
+                    print(f"        [*] {key2} : {pump[key][key2]}€/L")
+
---
+++ HolidayFinder.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+############################# [ IMPORTS ] #############################
+
+import pycountry, holidays, argparse, tabulate
+from datetime import date, timedelta
+
+############################# [ VARIABLES ] #############################
+
+lsDates = {
+    "Holiday name": [],
+    "From": [],
+    "To": [],
+    "Week #": [],
+    "Paid leaves": [],
+    "# of days off": []
+}  # Dict of data related to holidays
+
+days = ["Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday", "Sunday"]
+
+
+# ############################# [ FUNCTIONS ] #############################
+
+# Handling the args
+def getArgs():
+    parser = argparse.ArgumentParser("python3 HolidayFinder.py")  # Create an object to get args
+    # Options of this program
+    parser.add_argument("-Y", "-y", "--year", dest="year", help="Enter the year you want", type=int)
+    parser.add_argument("-C", "-c", "--code", dest="code", help="Enter the country/state code you are (if you know it)", type=str)
+    options = parser.parse_args()
+
+    if options.year is not None:  # Check if year option is empty
+        if not 1801 < options.year <= 9999 :  # Check if the year is supported
+            parser.error("\n    [-] Error in the command : please chose a year in the range ]1801;9999]")
+            parser.error("Check -h or --help for help")
+            exit()
+    else:
+        parser.error("\n    [-] Error in the command : please specify a year")
+        parser.error("Check -h or --help for help")
+        exit()
+
+    if options.code is not None:  # Check if code option is empty
+        if pycountry.countries.get(alpha_2=options.code) is not None :  # Check if it's a country code
+            if pycountry.subdivisions.get(country_code=options.code.split("-")[0]) is None :  # Check if it's a subdivision code
+                parser.error("\n    [-] Error in the command : please specify a valid state code or don't specify code")
+                parser.error("Check -h or --help for help")
+                exit()
+        else : 
+            parser.error("\n    [-] Error in the command : please specify a valid country code or don't specify code")
+            parser.error("Check -h or --help for help")
+            exit()
+
+    return options # options = Namespace object with attributes
+
+
+#--------------------------------------------------
+# Function to help user getting his country/state code
+def findCode():
+    # Display a list of available countries to the user
+    availableCountries = {}
+    for country in sorted(list(pycountry.countries),key=lambda x: x.alpha_2): 
+        availableCountries[country.alpha_2] = country.name 
+
+    print("\n[+] Available countries:\n")
+    for countryCode, countryName in availableCountries.items():
+        print(f"{countryCode}: {countryName}")
+
+    # Prompt the user to enter a country code
+    selectedCountryCode = input("\n[+] Enter the country code: ").upper()
+
+    # Check if the entered country code is valid
+    if selectedCountryCode not in availableCountries.keys():
+        print("\n[-] Invalid country code. Please enter a valid country code.")
+        findCode()
+    else: 
+        selectedCountry = pycountry.countries.get(alpha_2=selectedCountryCode)
+
+        subdivs = pycountry.subdivisions.get(country_code=selectedCountry.alpha_2)
+        # Check if there is state/province in all subdivisions
+        if any(subdiv.type == 'State' or subdiv.type == 'Province' for subdiv in subdivs): 
+            # Display a list of available state/province to the user
+            availableSubdivs = {}
+            for subdiv in sorted(list(subdivs),key=lambda x: x.code): 
+                availableSubdivs[subdiv.code] = subdiv.name
+
+            print("\n[+] Available subdivisions:\n")
+            for stateCode, stateName in availableSubdivs.items():
+                print(f"{stateCode}: {stateName}")
+
+            # Prompt the user to enter a country code
+            selectedSubdivCode = input("\n[+] Enter the subdivision code: ").upper()
+            
+            # Retrieve the holidays for the selected subdivision (if possible)
+            if selectedSubdivCode not in availableSubdivs.keys():
+                print("\n[-] Invalid state/province code. Please enter a valid code.")
+                findCode()
+            else :
+                print(f"\n[+] Your code is : {selectedSubdivCode}")
+                return selectedSubdivCode # selectedSubdivCode = str
+        else :
+            print(f"\n[+] Your code is : {selectedCountryCode}")
+            return selectedCountryCode # selectedCountryCode = str
+
+
+#--------------------------------------------------
+# Function to get holidays days depending on the country/state code
+def findHolidays(year,code):
+    if "-" in code:  # Checking if subdivision code 
+        countryCode,subdivCode = code.split("-")
+        holidaysList =  holidays.CountryHoliday(countryCode, state=code, years=year)
+    else:
+        countryCode = code
+        holidaysList = holidays.CountryHoliday(countryCode, state=None, years=year)
+
+    # Setting holidays' name in the list of holidays
+    filteredHolidays = holidaysList.copy()
+    for date in holidaysList.items(): 
+        if 0 <= date[0].weekday() <= 4:  # Check if the day is in the weekdays (weekend day is useless to look up)
+            lsDates["Holiday name"].append(date[1])
+        else :
+            filteredHolidays.pop(date[0])
+    return filteredHolidays  # filteredHolidays = {datetime object: str}
+
+#--------------------------------------------------
+# Function to calculate ranges for taking paid leaves
+def makeRanges(dateList):
+    rangesCoeff = []
+    for date in dateList.items():  # Calculate number of days to add or subtract to have a range
+        # print(days[date[0].weekday()])
+        if date[0].weekday() == 0:  # Monday
+            rangesCoeff.append((0, 4))  # Before and after weekdays left
+        elif date[0].weekday() == 1:  # Tuesday
+            rangesCoeff.append((1, 3))
+        elif date[0].weekday() == 2:  # Wednesday
+            rangesCoeff.append((2, 2))
+        elif date[0].weekday() == 3:  # Thursday
+            rangesCoeff.append((3, 1))
+        else:  # Friday
+            rangesCoeff.append((4, 0))
+
+    rangesDates = []
+    for i in range(len(rangesCoeff)):
+        currentHoliday = list(dateList.items())[i][0]  # datetime object of the list of tuple
+        startDay = currentHoliday - timedelta(days=rangesCoeff[i][0])  # Start paid leave to take
+        endDay = currentHoliday + timedelta(days=rangesCoeff[i][1])  # End paid leave to take
+
+        if startDay == currentHoliday:  # If holiday is Monday >> no need to take days before
+            tmp = currentHoliday + timedelta(days=1)
+            rangesDates.append((tmp, endDay))
+        elif endDay == currentHoliday:  # If holiday is Friday >> no need to take days after
+            tmp = currentHoliday - timedelta(days=1)
+            rangesDates.append((startDay, tmp))
+        else:  # In any other cases the range is correct
+            rangesDates.append((startDay, endDay))
+
+    return rangesDates  # rangesDates = [(datetime object, datetime object),...]
+
+
+#--------------------------------------------------
+# Function to make a clean table
+def makeTabulate(rangesDateList):
+    for date in rangesDateList:
+        lsDates["From"].append(date[0])  # Start date
+        lsDates["To"].append(date[1])  # End date
+        lsDates["Week #"].append(date[0].isocalendar()[1])  # Week number
+        deltadays = date[1]-date[0]
+        lsDates["Paid leaves"].append(deltadays.days+1)  # Number of paid leaves to take
+        lsDates["# of days off"].append(deltadays.days+1+4)  # Number of off days (+4 => 2 weekends surrounding the week)
+
+
+#--------------------------------------------------
+# Function to print a pretty table 
+def tablePrint():
+    return str(tabulate.tabulate(lsDates, headers="keys", tablefmt="github"))  # Make a pretty print
+
+
+############################# [ LAUNCH ] #############################
+
+options = getArgs()
+if options.code is None :
+    options.code = findCode()
+
+dateList = findHolidays(options.year,options.code)
+rangesDateList = makeRanges(dateList)
+makeTabulate(rangesDateList)
+
+#time.sleep(0.052)
+#time.sleep(0.045)
+
+print(tablePrint())
